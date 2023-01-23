fe6c37c4419d3ddd3ac943ced0ae322a9dd540e8
unknown <luamgebremikael@gmail.com>
2023-01-23 15:00:13 -0500

    first commit

---
+++ chapter3/chapter 3.py
@@ -0,0 +1,49 @@
+my_list=[]
+print(type(my_list))
+my_list=list()
+print(type(my_list))
+
+# index    0 , 1 ,2 ,3 ,4
+numbers = [2, 6, 10, 12,5, 0]#this is also a comment
+print(numbers) 
+print(type(numbers))
+
+print(numbers[1]) 
+print(type(numbers[1]))
+print(numbers[1]*2.5)
+print(type(numbers[1]*2.5))
+print(numbers[3]+0.5)
+print(type(numbers[3])) 
+#index    1,         2,     3,     4
+foods = ["injera","rice","cake","pasta"]
+
+
+print(foods[1])
+print(type(foods[1]))
+print(foods[1].upper())
+x=12
+y=15.6
+z="z"
+random_list=[x,y,z]
+foods =["injera","rice","cake","pasta"]
+print(foods.append("cheeseburger"))
+print(foods)
+foods .insert(0,"pha")
+print(foods)
+foods.remove("pha")
+print(foods)
+foods.remove("byurrito")
+# index   0,        1,     2,     3
+foods = ["injera","rice","cake","pasta"]
+print (foods.pop())
+print(foods)
+#index    1,         2,     3,     4
+foods = ["injera","rice","cake","pasta"]
+print(foods.pop())
+
+
+
+#index    1,         2,     3,     4
+foods = ["injera", "rice", "cake", "pasta"]
+foods.append('injera')
+print(foods)
---
+++ chapter4/chapter4.py
@@ -0,0 +1,2 @@
+my_list=[1,3.0,["a","b",["A","b","c"],"d"],"Jhon"]
+print(my_list[2][2][0])
